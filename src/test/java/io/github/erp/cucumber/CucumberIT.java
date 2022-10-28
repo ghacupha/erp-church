ff130613094ac293c7da95a27455702d779787c4
Edwin Njeru <mailnjeru@gmail.com>
2022-10-28 14:50:35 +0300

    Initial version of erpChurch generated by generator-jhipster@7.9.3

---
+++ src/test/java/io/github/erp/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package io.github.erp.cucumber;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+import io.github.erp.IntegrationTest;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
---
+++ src/test/java/io/github/erp/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,8 @@
+package io.github.erp.cucumber.stepdefs;
+
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+public abstract class StepDefs {
+
+    protected WebTestClient.ResponseSpec actions;
+}
