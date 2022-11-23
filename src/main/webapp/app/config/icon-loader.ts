048448d7ee5fe7092663a6bc90035b076c5ce24f
Edwin Njeru <mailnjeru@gmail.com>
2022-11-23 08:50:14 +0300

    Create base license header

--- src/main/webapp/app/config/icon-loader.ts
+++ src/main/webapp/app/config/icon-loader.ts
@@ -1,3 +1,21 @@
+///
+/// Erp Church - Data management for religious institutions
+/// Copyright © 2022 Edwin Njeru (mailnjeru@gmail.com)
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///
+
 import { faCogs } from '@fortawesome/free-solid-svg-icons/faCogs';
 import { faBan } from '@fortawesome/free-solid-svg-icons/faBan';
 import { faAsterisk } from '@fortawesome/free-solid-svg-icons/faAsterisk';
